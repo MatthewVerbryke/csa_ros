56a00592db0a098ae5b03b3ba551b326359dc88c
Matthew Verbryke <verbrymr@mail.uc.edu>
2024-04-18 16:24:22 -0400

    Add catch for strings when using literal_eval

--- csa_msgs/src/csa_msgs/params.py
+++ csa_msgs/src/csa_msgs/params.py
@@ -106,9 +106,10 @@ def key_value_list_to_dict(kv_list):
     
     # Evaluate each key-value pair and store in output dictionary
     for entry in kv_list:
+        
         try:
             interp_value = ast.literal_eval(entry.value)
-        except:
+        except ValueError:# TODO: Fix this: catches strings, but not ideal
             interp_value = entry.value
         
         dict_out.update({entry.key: interp_value})
